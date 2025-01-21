39421f85ff0fe1dd452842af7a50dbe54d252790
deemon1988 <dmn72835@gmail.com>
2025-01-21 21:47:02 +0300

    Задача "Асинхронные силачи"

---
+++ 13_1.py
@@ -0,0 +1,13 @@
+import time
+import asyncio
+
+async def notification():
+    time.sleep(10)
+    print("До доставки осталось 10 минут")
+
+async def main():
+    task = asyncio.create_task(notification())
+    print("Собираемся в поездку")
+    print("Едим")
+
+asyncio.run(main())
